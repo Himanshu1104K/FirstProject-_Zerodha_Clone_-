7db9dfe0880efab7c6df7e28ee0b62b5aec36a45
Himanshu1104K <honey6k6@gmail.com>
2024-09-30 16:59:02 +0530

    Pricing Page Completed

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>
-            Hero
+  return (
+    <div className="container mb-5">
+      <div className="text-center m-5 p-5">
+        <h1 style={{ fontSize: "2.75rem" }} className="my-3">
+          Charges
         </h1>
-     );
+        <h5 style={{ color: "#9b9b9b" }}>List of all charges and taxes</h5>
+      </div>
+      <div className="row text-center">
+        <div className="col-1"></div>
+        <div className="col">
+          <img src="images\pricing0.svg" />
+          <h2 className="my-3">Free equity delivery</h2>
+          <p style={{ color: "#9b9b9b" }} >
+            All equity delivery investments (NSE, BSE), are absolutely free — ₹
+            0 brokerage.
+          </p>
+        </div>
+        <div className="col">
+          <img src="images\intradayTrades.svg" />
+          <h2 className="my-3">Intraday and F&O trades</h2>
+          <p style={{ color: "#9b9b9b" }}>
+            Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
+            intraday trades across equity, currency, and commodity trades. Flat
+            ₹20 on all option trades.
+          </p>
+        </div>
+        <div className="col">
+          <img src="images\pricing0.svg" />
+          <h2 className="my-3">Free direct MF</h2>
+          <p style={{ color: "#9b9b9b" }}>
+            All direct mutual fund investments are absolutely free — ₹ 0
+            commissions & DP charges.
+          </p>
+        </div>
+        <div className="col-1"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Hero from "./Hero";
-import Brokerage from "./Brokerage";
 
 function PricingPage() {
   return (
     <>
       <Hero />
-      <Brokerage />
     </>
   );
 }
